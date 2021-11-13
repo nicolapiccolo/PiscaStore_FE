234b3e5f6d6cd469e443133ac3b979bda70ebbdc
Nicola Piccolo <nikola9piccolo9@gmail.com>
2021-11-13 10:12:50 +0100

    Test listing category from DB

--- pisca/src/app/app.module.ts
+++ pisca/src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import {CategoryService} from "./service/category.service";
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            '88968581162-nn91gvn74h74nu40ald23ss6faq5dk3a.apps.googleusercontent.com'
+            )
+         }
+        ]
+      } as SocialAuthServiceConfig,
+    },
+    CategoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
