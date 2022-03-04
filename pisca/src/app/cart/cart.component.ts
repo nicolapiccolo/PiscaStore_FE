f76579593d9126906433aaea5ede9b5251fafa5b
Nicola Piccolo <nikola9piccolo9@gmail.com>
2022-03-04 17:26:18 +0100

    Implemented create order on checkout cart

--- pisca/src/app/cart/cart.component.ts
+++ pisca/src/app/cart/cart.component.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Product} from "../model/product";
 import {CartService} from "../service/cart.service";
 import {ProductService} from "../service/product.service";
 import {LoadingService} from "../service/loading.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {formatNumber} from "@angular/common";
+import {AccountService} from "../service/account.service";
+import {OrderUser} from "../model/orderUser";
+import {Item} from "../model/item";
+import {HttpResponse} from "@angular/common/http";
+import {OrderService} from "../service/order.service";
+import {NgbModal, NgbModalOptions} from "@ng-bootstrap/ng-bootstrap";
+import {NgbdModalProgress} from "../product-insert/product-insert.component";
+import {ModalConfirm} from "../modal/modalConfirm";
 
 @Component({
   selector: 'app-cart',
@@ -13,6 +21,7 @@ import {formatNumber} from "@angular/common";
 })
 export class CartComponent implements OnInit {
 
+  currentUser: number = 0;
   products?: Array<Product>;
   isEmpty = true;
   size = this.products?.length;
@@ -24,48 +33,139 @@ export class CartComponent implements OnInit {
   constructor(public loader: LoadingService,
               private route: ActivatedRoute,
               private router: Router,
-              private cartService: CartService) {
+              private cartService: CartService,
+              private accountService: AccountService,
+              private orderService: OrderService,
+              private modalService: NgbModal) {
 
   }
 
 
   ngOnInit(): void {
-    this.products = this.cartService.getProducts()
-    if (this.products.length>0){
-      this.isEmpty = false;
-      this.size = this.products?.length;
-      this.getTotalItems();
-      this.getTotalPrice();
-    }
-    console.log(this.isEmpty)
+
+    this.accountService.getCurrentUser().subscribe((data: any) => {
+        console.log("user: ", data)
+        if (data <= 0 || data == undefined || data == null) {
+          this.currentUser = 0
+          console.log("cannot retrive user")
+        } else {
+          this.currentUser = data
+          this.products = this.cartService.getProducts()
+          if (this.products.length > 0) {
+            this.isEmpty = false;
+            this.size = this.products?.length;
+            this.getTotalItems();
+            this.getTotalPrice();
+          }
+          console.log(this.isEmpty)
+        }
+      },
+      (err: any) => {
+        console.log(err)
+        this.currentUser = 0
+        console.log("cannot retrive user")
+        this.router.navigateByUrl("/login")
+      });
   }
 
-  getTotalItems(): string{
-    this.strTotal = this.size+" item"
-    if(this.size!! > 1) this.strTotal += "s"
+  openProgress(message: string) {
+    let ngbModalOptions: NgbModalOptions = {
+      backdrop: 'static',
+      keyboard: false
+    };
+    const modalRef = this.modalService.open(NgbdModalProgress, ngbModalOptions);
+    modalRef.componentInstance.message = message;
+    return modalRef
+  }
+
+  closeProgressModal(modalRef: any) {
+    modalRef.close();
+  }
+
+
+  getTotalItems(): string {
+    this.strTotal = this.size + " item"
+    if (this.size!! > 1) this.strTotal += "s"
     return this.strTotal
   }
 
-  getTotalPrice(){
-    for(let product of this.products!!){
-        this.totalPrice += product.price
+  getTotalPrice() {
+    for (let product of this.products!!) {
+      this.totalPrice += product.price
 
     }
   }
 
-  removeItem(product: Product){
+  removeItem(product: Product) {
     const index: number = this.products!!.indexOf(product)
     this.products?.splice(index, 1)
     console.log(this.products?.length)
     window.location.reload()
   }
 
-  createOrder(){
-    let items_id = new Array<number> ();
+  async createOrder() {
 
-    for(let product of this.products!!){
-      items_id.push(product.id)
-    }
+    if (this.currentUser > 0) {
+
+      if (this.products != undefined && this.products.length > 0) {
+
+        const modalRef = this.modalService.open(ModalConfirm);
+        modalRef.componentInstance.title = "Conferma ordine"
+        modalRef.componentInstance.name = "";
+        modalRef.componentInstance.message1 = "Vuoi confermare l'ordine ";
+
+        modalRef.componentInstance.message2 = "L'articolo non sarà più disponibile all'acquisto sulla nostra piattaforma";
+
+        modalRef.result.then((data) => {
+
+          const progress = this.openProgress("Creazione ordine in corso")
+
+          let items = new Array<Item>();
+
+          for (let product of this.products!!) {
+            items.push(new Item(product.name, Number(product.price), product.id))
+          }
+
+          const order = new OrderUser(this.currentUser, items)
+
+
+          console.log("ORDER: ", order);
+          this.orderService.create(order).subscribe(
+            (event: any) => {
+              progress.close()
+              if (event instanceof HttpResponse) {
+                console.log(event.body.message)
+                const id = event.body.message
+
+                //if(modalRef) this.closeProgressModal(modalRef)
+
+                //this.router.navigateByUrl('catalog/' + id);
+              }
+            },
+            (err: any) => {
+              progress.close()
+              //if(modalRef) this.closeProgressModal(modalRef)
+              if (err.error && err.error.message) {
+                console.log(err.error.message);
+                //this.errorModal(err.error.message);
+
+              } else {
+                console.log('Error during creation')
+                //this.errorModal('Generic Error');
+              }
+            }
+          )
+
+        }, (reason) => {
+          // on dismiss x close
+          console.log("dismiss")
+        });
+
+      }
+
+    } else console.log("Invalid user")
+
+
   }
 
 }
