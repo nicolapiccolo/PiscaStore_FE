d562ecf4006be90f4175ee175a45e88625bdf6d1
FRANK <frank.sca98@gmail.com>
2022-03-10 11:06:34 +0100

    landing page

--- pisca/src/app/service/account.service.ts
+++ pisca/src/app/service/account.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import { Observable } from 'rxjs';
+import {Address} from "../model/address";
 
 const AUTH_API = 'http://localhost:8081/account/';
 
@@ -18,7 +19,14 @@ export class AccountService {
   }
 
   getAddresses(): Observable<any>{
-    return this.http.get(AUTH_API + 'address',httpOptions);
+    let add = this.http.get(AUTH_API + 'address',httpOptions);
+    console.log("indirizzi trovati ",add);
+    return add;
+  }
+
+  setAddress(data: any): Observable<any>{
+    console.log(data)
+    return this.http.put(AUTH_API + 'create',data,httpOptions);
   }
 
   getCurrentUser(): Observable<any> {
